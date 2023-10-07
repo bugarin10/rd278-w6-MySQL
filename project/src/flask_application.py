99ebaa1db84403faf3a61bd574d835f7b548c28a
Rafael Davila-Bugarin <rafaeldavila.b@gmail.com>
2023-10-07 13:29:32 -0400

    upgrading with wtf

--- project/src/flask_application.py
+++ project/src/flask_application.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
 
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, SubmitField
-# from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create flask instance
 app = Flask(__name__)
 
-# app.config["SECRET_KEY"] = "123"
+app.config["SECRET_KEY"] = "123"
 
 
 # Create a Form Class
-# class NamerForm(FlaskForm):
-#    name = StringField("Write your query", validators=[DataRequired()])
-#    submit = SubmitField("Submit")
+class QueryForm(FlaskForm):
+    query_by_user = StringField("Write your query", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 # Create a URL route in our application for "/"
@@ -23,6 +23,13 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/make_query")
+@app.route("/make_query", methods=["GET", "POST"])
 def query():
-    return render_template("query.html")
+    query_by_user = None
+    form = QueryForm()
+
+    if form.validate_on_submit():
+        query_by_user = form.query_by_user.data
+        form.query_by_user.data = ""
+
+    return render_template("query.html", form=form, query_by_user=query_by_user)
