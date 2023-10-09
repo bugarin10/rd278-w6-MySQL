fff511d2c2fc1f5a860362352563fe65e5047ab2
Rafael Davila-Bugarin <rafaeldavila.b@gmail.com>
2023-10-09 16:45:20 -0400

    successing

--- project/src/app.py
+++ project/src/app.py
@@ -1,8 +1,41 @@
+import mysql.connector
+import credentials
+
 from flask import Flask, render_template
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
+
+def doing_query(query_by_user):
+    try:
+        cnx = mysql.connector.connect(
+            user="myadmin",
+            password=credentials.password,
+            host="mysqlserverrd278.mysql.database.azure.com",
+            port=3306,
+            database="vet_schema",
+        )
+
+        cursor = cnx.cursor()
+
+        cursor.execute(query_by_user)
+
+        results = cursor.fetchall()
+
+        columns = cursor.column_names
+
+        n_columns = len(columns)
+
+        # for row in results:
+        #    print(row)
+        print(columns)
+        cursor.close()
+        cnx.close()
+        return results, columns, n_columns
+    except:
+        raise Exception("Something went wrong with the query")
 
 
 # Create flask instance
@@ -32,4 +65,18 @@ def query():
         query_by_user = form.query_by_user.data
         form.query_by_user.data = ""
 
-    return render_template("query.html", form=form, query_by_user=query_by_user)
+    if query_by_user is None:
+        results = None
+        columns = None
+        n_columns = None
+    else:
+        results, columns, n_columns = doing_query(query_by_user)
+
+    return render_template(
+        "query.html",
+        form=form,
+        query_by_user=query_by_user,
+        results=results,
+        columns=columns,
+        n_columns=n_columns,
+    )
